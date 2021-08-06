11e5b48b6f2f634ec937f2c247259a8d12c37805
aaron-might <aaronmighty7@gmail.com>
2021-08-06 16:52:24 -0400

    update about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -26,7 +26,7 @@ const {PUBLIC_URL} = process.env;
                     <div className="col-md-8" type="text">
                       
                       <p> 
-                      Aaron Mighty live in Charlotte, NC. I am studying Web development at UNCC bootcamp. I am passionate about engineering and technology. I'm an associate Industrial Engineering with Associate Degree from South Africa. I had an interest into computer since teenagers but decided to do engineering and automation instead. now as it where the world is going there is huge need of IT graduates i like programming 
+                      Aaron Mbuyi live in Charlotte, NC. I am studying Web development at UNCC bootcamp. I am passionate about engineering and technology. I'm an associate Industrial Engineering with Associate Degree from South Africa. I had an interest into computer since teenagers but decided to do engineering and automation instead. now as it where the world is going there is huge need of IT graduates i like programming 
 
                       other than that I love music,traveling,experience with new things. I multi-lingual so as speak many language apart from the french, and english which are common.
                       </p>
